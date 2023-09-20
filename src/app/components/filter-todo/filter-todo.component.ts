f10cb70e83832d20a252e3d54150b469d49a32cd
Gian Rafael Thiam <gian.thiam01@gmail.com>
2023-09-20 12:10:43 +0800

    disabled filters when there are no items that match that filter

--- src/app/components/filter-todo/filter-todo.component.ts
+++ src/app/components/filter-todo/filter-todo.component.ts
@@ -1,24 +1,43 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { Todo } from "src/app/models/todo";
 import { FilterOptions, TodoFilterService } from "src/app/todo-filter.service";
+
+import { Observable, of } from "rxjs";
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-filter-todo",
   templateUrl: "./filter-todo.component.html",
   styleUrls: ["./filter-todo.component.scss"],
 })
-export class FilterTodoComponent {
+export class FilterTodoComponent implements OnInit {
   filters: FilterOptions = {
     low: true,
     medium: true,
     high: true,
   };
 
+  @Input() todos$: Observable<Todo[]> = new Observable();
+  inputState$: Observable<FilterOptions> = new Observable();
+
   private controlPressed: boolean = false;
 
-  constructor(private filterService: TodoFilterService) {
-    this.filters = filterService.filters.value;
+  constructor(private filterService: TodoFilterService) {}
+
+  ngOnInit() {
+    this.filters = this.filterService.filters.value;
     document.addEventListener("keydown", this.handleKey.bind(this));
     document.addEventListener("keyup", this.handleKey.bind(this));
+
+    this.inputState$ = this.todos$.pipe(
+      switchMap((todos: Todo[]) =>
+        of({
+          low: todos.some((todo) => todo.priority === "low"),
+          medium: todos.some((todo) => todo.priority === "medium"),
+          high: todos.some((todo) => todo.priority === "high"),
+        })
+      )
+    );
   }
 
   handleKey(event: KeyboardEvent) {
